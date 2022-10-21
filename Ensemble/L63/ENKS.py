1f67589f362e5c651321123c1c3ee7411e7c79c9
Zihong Xu <zxu455@wisc.edu>
2022-10-20 21:33:29 -0500

    update

--- Ensemble/L63/ENKS.py
+++ Ensemble/L63/ENKS.py
@@ -8,6 +8,7 @@ Created on Thu Oct 20 00:16:21 2022
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.random import multivariate_normal as MN
+from numpy.linalg import inv
 """
 ---------------------------------------------------------------------------------------------------------------------
 Overview:
@@ -41,8 +42,8 @@ Parameters:
         
     
 """
-class ENKS:
-    def init(self, x_init, x_dim, o_dim, ens_num, P_p, P_o, H):
+class ENKS():
+    def __init__(self, x_init, x_dim, o_dim, ens_num, P_p, P_o, H, t2o, pred, dt):
         self.x = x_init
         self.x_dim = x_dim
         self.o_dim = o_dim
@@ -50,8 +51,42 @@ class ENKS:
         self.P_p = P_p
         self.P_o = P_o
         self.H = H
+        self.t2o = t2o
+        self.pred = pred
+        self.dt = dt
+        self.x_ens = MN(self.x, cov = self.P_p, size = self.ens_num)
+        self.o_ens = np.zeros((ens_num,o_dim))
+        self.mean = np.zeros(self.x_dim)
+        self.mean_o = np.zeros(self.o_dim)
+    def cov(self,A,B):
+        C = 0
+        for i in range(len(A)):
+            ctemp = np.outer(A[i], B[i])
+            C += ctemp
+            
+        return C
         
-    def ENKF(self):
-        x_ens = MN(self.x, cov = self.P_p, size = self.ens_num)
-        for i in range(len(x_ens)):
-            x_ens[i] = 
+    def ENKF(self, o):
+        # predict state ensemble and find its prediction covariance matrix
+        for i,ee in enumerate(self.x_ens):
+            self.x_ens[i] = self.pred(ee,self.dt)
+        self.x = np.mean(self.x_ens, axis = 0)
+        self.P_p = self.cov(self.x_ens-self.x, self.x_ens-self.x)/(self.ens_num-1)        
+        
+        # data ensemble construction
+        for i in range(self.ens_num):
+            self.o_ens[i] = self.t2o(self.x_ens[i])   
+        o_mean = np.mean(self.o_ens, axis = 0)
+        P_oe = np.eye(self.o_dim)*10
+        
+        # perform the analysis/filtering step, construct the analyzed state covariance matrix
+        K = self.P_p@self.H.T@(inv(self.H@self.P_p@self.H.T + P_oe))
+        for i,ee in enumerate(self.x_ens):
+            self.x_ens[i] = ee + K@(self.o_ens[i] - self.H@ee)
+            # if observation dimension is larger than the ensemble number, the first part in the bracket will be singular.
+        
+        self.P_p = self.P_p - K@self.H@self.P_p
+        
+        # take the mean of the posterior enssemble as posterior state
+        self.x = np.mean(self.x_ens, axis = 0)
+        return self.x, self.P_p,self.x_ens
